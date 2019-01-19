2a1ad34c10b8865dcec0f66ccbbc60d4ec9988e6
Stefan Kreuter <info@kreutix.de>
2019-01-19 11:12:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cookie Notice Accepted
+
+## What is Cookie Notice Accepted?
+
+Cookie Notice Accepted minimalistic extension which sets cookies to tell your websites that you have accepted the cookie notice, so it wont ask you again.
+
+## Supported "cookie notice accepted" cookies
+
+- cookie_notice_accepted = true
+- cookieconsent_status = dismiss
---
+++ background.js
@@ -0,0 +1,77 @@
+/* global chrome */
+
+chrome.runtime.onInstalled.addListener(welcomePage);
+chrome.browserAction.onClicked.addListener(toogleExtension);
+chrome.webNavigation.onBeforeNavigate.addListener(updateCookies);
+
+var cookies = {
+	'cookie_notice_accepted': 'true',
+	'cookieconsent_status': 'dismiss',
+}
+
+var enabled = false;
+chrome.storage.sync.get(null, function(obj) {
+	enabled = obj.enabled;
+});
+
+function welcomePage(details){
+    if (details.reason == 'install'){
+		chrome.tabs.create({
+			url: 'options.html',
+			active: true
+		});
+	}
+}
+
+function toogleExtension(tab){
+	if(!enabled){
+		enableExtension();
+		updateCookies(tab);
+	} else {
+		disableExtension();
+		updateCookies(tab);
+	}
+}
+
+function updateCookies(tab) {
+	if (tab.url.indexOf('http') === 0) {
+		for (var key in cookies) {
+			if (enabled) {
+				var expire = new Date/1E3|0;
+					expire += 60*60*24*30;
+				chrome.cookies.set({
+					url: tab.url,
+					name: key,
+					value: cookies[key],
+					expirationDate: expire
+				});
+			} else {
+				chrome.cookies.remove({
+					url: tab.url,
+					name: key,
+				});
+			}
+		}
+	}
+}
+
+function enableExtension() {
+	enabled = true;
+	chrome.browserAction.setIcon({ path: {'19': 'images/icon19.png', '38': 'images/icon38.png'} });
+	chrome.browserAction.setTitle({ title: 'Disable Cookie Notice accepted' });
+	updateStore();
+}
+
+function disableExtension() {
+	enabled = false;
+	chrome.browserAction.setIcon({ path: {'19': 'images/icon19-gray.png', '38': 'images/icon38-gray.png'} });
+	chrome.browserAction.setTitle({ title: 'Enable Cookie Notice accepted' });
+	updateStore();
+}
+
+function updateStore() {
+	chrome.storage.sync.get(null, function(obj) {
+		obj.enabled = enabled;
+		chrome.storage.sync.set(obj);
+	});
+}
